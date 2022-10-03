4fec2eb50397fa187c6abff7722c85758004de10
hermionearcher <46831152+hermionearcher@users.noreply.github.com>
2022-10-03 13:26:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ts-projects
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-v6gjfq)
---
+++ src/projects/ProjectsPage.tsx
@@ -0,0 +1,8 @@
+const ProjectsPage = () => {
+  return (
+    <blockquote cite="Benjamin Franklin">
+      Tell me and I forget, teach me and I may remember, involve me and I learn.
+    </blockquote>
+  );
+};
+export default ProjectsPage;
